539bc1594458c3a8e6d69190c5cf6293c49c6955
temX-tech <81012799+temX-tech@users.noreply.github.com>
2021-03-20 14:27:32 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,68 @@
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+
+function correctAnswer(){
+        correctAnswers = correctAnswers + 1;
+        alert('Правильно!')
+}
+function incorrectAnswer(message){
+        incorrectAnswers = incorrectAnswers + 1;
+        alert('Не правильно! \nПравильный ответ "' + message + '"');
+}
+
+if(prompt("Компьютер на английском языке...") == 'computer'){
+        correctAnswer();
+
+} else {
+        incorrectAnswer('computer');
+        
+}
+
+if(prompt("Звук на английском языке...") == 'sound'){
+        correctAnswer();
+
+} else {
+        incorrectAnswer('sound');
+        
+}
+
+if(prompt("Дом на английском языке...") == 'home'){
+        correctAnswer();
+} else {
+        incorrectAnswer('home');
+}
+
+
+if(prompt("Ноутбук на английском языке...") == 'laptop'){
+        correctAnswer() 
+    } else {
+        incorrectAnswer('laptop');
+    }
+if(prompt("ПК на английском языке...") == 'PC'){
+        correctAnswer();
+
+} else {
+        incorrectAnswer('PC');
+        
+}
+if(prompt("Привет на английском языке...") == 'Hello'){
+        correctAnswer();
+} else {
+        incorrectAnswer('Hello');
+}
+
+if(prompt("Играть на английском языке...") == 'Play'){
+        correctAnswer();
+} else {
+        incorrectAnswer('Play');
+}
+if(prompt("можно? на английском языке...") == 'Can I?'){
+        correctAnswer();
+} else {
+        incorrectAnswer('Can I?');
+}
+
+
+
+    alert('У вас ' + correctAnswers + ' правильных ответов')
+    alert('У вас ' + incorrectAnswers + ' не правильных ответов')
